d3cacccc10c3cc11e2c4f98d7a079ab3e8db86dc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-11-25 18:05:31 +0100

    Update Employee.kt

--- src/main/kotlin/local/example/demo/model/Employee.kt
+++ src/main/kotlin/local/example/demo/model/Employee.kt
@@ -27,14 +27,17 @@ class Employee {
 
     @Id
     @GeneratedValue
-    var id: Int? = null
+    var id: Long? = null
 
     var name: String? = null
     var surname: String? = null
 
     constructor()
 
-    constructor(name: String, surname: String) {
+    constructor(
+            name: String?,
+            surname: String?
+    ) {
         this.name = name
         this.surname = surname
     }
